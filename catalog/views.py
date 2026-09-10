4f9c130e3bb9d2d56a67ec46d67cf8dfbfbc2bf5
AlexSen81 <smolgrand@bk.ru>
2026-09-10 12:52:17 +0300

    Создано приложение catalog и настроена главная страница

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse  # Импортируем HttpResponse, чтобы вернуть простой текст
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Добро пожаловать в мастерскую ЯсеньStudio!</h1><p>Здесь скоро будет каталог наших изделий.</p>")
