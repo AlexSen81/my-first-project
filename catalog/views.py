5bbb9a072f9e9510c1f022164a1931ef456aac9c
AlexSen81 <smolgrand@bk.ru>
2026-09-10 13:50:48 +0300

    Добавлена страница Контакты и настроена навигация

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+# Эта функция ОБЯЗАТЕЛЬНО должна быть здесь:
 def index(request):
-    # Теперь мы не пишем HTML в коде, а вызываем готовый шаблон из папки templates
     return render(request, 'catalog/index.html')
+
+# Наша новая функция:
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
