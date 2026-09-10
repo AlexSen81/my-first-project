13a0307f3df40ece561d917bebb4653f11b2698e
AlexSen81 <smolgrand@bk.ru>
2026-09-10 13:26:27 +0300

    Настроена система шаблонов и создана главная страница

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse  # Импортируем HttpResponse, чтобы вернуть простой текст
 
 def index(request):
-    return HttpResponse("<h1>Добро пожаловать в мастерскую ЯсеньStudio!</h1><p>Здесь скоро будет каталог наших изделий.</p>")
+    # Теперь мы не пишем HTML в коде, а вызываем готовый шаблон из папки templates
+    return render(request, 'catalog/index.html')
